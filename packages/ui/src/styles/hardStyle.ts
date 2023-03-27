539142c707710c77f46a80dc2f8683f3e700c216
dinoDanic <dino.danic@gmail.com>
2023-03-27 15:07:21 +0200

    test

--- packages/ui/src/styles/hardStyle.ts
+++ packages/ui/src/styles/hardStyle.ts
@@ -2,6 +2,8 @@ import { CSSProperties } from "@vanilla-extract/css";
 import React from "react";
 import { BoxProps } from "../components/primitives/Box/Box";
 
+//test
+
 export interface HardStyles {
   __margin?: CSSProperties["margin"] | number;
   __padding?: CSSProperties["padding"] | number;
