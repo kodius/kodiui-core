ea836228dfc59ae671c65c1ebcd9f245685199c8
Matija Sporis <matija.sporis@contractors.evenfinancial.com>
2023-03-27 13:20:48 +0200

    add vanilla extract plugin to vitest config

--- packages/ui/src/components/primitives/Box/Box.test.tsx
+++ packages/ui/src/components/primitives/Box/Box.test.tsx
@@ -3,11 +3,8 @@ import { render, screen } from "@testing-library/react";
 import { Box } from "./Box";
 
 describe("Box", () => {
-  it("renders headline", () => {
-    render(<Box>box</Box>);
-
-    screen.debug();
-
-    // check if App components renders headline
+  it("renders children", () => {
+    render(<Box>foo bar baz</Box>);
+    expect(screen.getByText(/foo/i)).toBeInTheDocument();
   });
 });
--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -2,13 +2,14 @@
 
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
+    setupFiles: "./src/tests/setup.ts",
   },
 });
