9d19fc8131674632c354d82028cb39e471aac244
dinoDanic <dino.danic@gmail.com>
2023-04-19 15:02:31 +0200

    new components

--- apps/docs/src/components/index.ts
+++ apps/docs/src/components/index.ts
@@ -1,11 +1,11 @@
 export { Ident } from "./layout/Ident";
 export { Heading } from "./typography/Heading/Heading";
-export { Text } from "./typography/Text";
+export { Text } from "./typography/Text/Text";
 
-export { Button } from "./input/Button";
+export { Button } from "./input/Button/Button";
 export { type ButtonVariants } from "./input/Button/button.css";
 
 export { Drawer } from "./ui/Drawer";
-export { TextLink } from "./input/TextLink";
+export { TextLink } from "./input/TextLink/TextLink";
 
 export { Input } from "./input/Input/Input";
