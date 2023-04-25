ce3b0915afb8b878c09d8eee90a9250fedc230e5
antebaskovic <119291043+antebaskovic@users.noreply.github.com>
2023-04-25 09:43:33 +0200

    stack

--- apps/docs/src/pages/primitives/stack/index.tsx
+++ apps/docs/src/pages/primitives/stack/index.tsx
@@ -17,70 +17,42 @@ const StackPage = () => {
   return (
     <Doc>
       <Doc.Title>Stack</Doc.Title>
-      <Doc.Subtitle>Gap</Doc.Subtitle>
-      <Doc.Example>
-        <Doc.Checkboxes
-          checkboxes={gapCheckboxes}
-          setCheckboxes={setGapCheckboxes}
-        >
-          {(checked) => {
-            const codeSnippet = `<Stack gap={${checked?.value}} alignItems="center">
-                <Doc.Placeholder width="full" />
-                <Doc.Placeholder width="full" />
-                <Doc.Placeholder width="full" />
-</Stack>`;
-            return (
-              <Stack gap={"xs"}>
-                <Stack gap={checked?.value} alignItems="center">
-                  <Doc.Placeholder width="full" />
-                  <Doc.Placeholder width="full" />
-                  <Doc.Placeholder width="full" />
-                </Stack>
-                <Doc.CodeSnippet
-                  codeSnippet={codeSnippet}
-                  showLineNumbers={false}
-                />
-              </Stack>
-            );
-          }}
-        </Doc.Checkboxes>
-      </Doc.Example>
 
-      <Doc.Subtitle>Alignment</Doc.Subtitle>
-      <Doc.Example>
-        <Doc.Checkboxes
-          checkboxes={alignCheckboxes}
-          setCheckboxes={setAlignCheckboxes}
-        >
-          {(checked) => {
-            const codeSnippet = `<Stack alignItems={${checked?.value}}>
-             <Doc.Placeholder width="full" />
-             <Doc.Placeholder width="full" />
-             <Doc.Placeholder width="full" />
-</Stack>`;
-            return (
-              <Stack gap={"xs"}>
-                <Stack alignItems={checked?.value}>
-                  <Doc.Placeholder width="6/12" />
-                  <Doc.Placeholder width="6/12" />
-                  <Doc.Placeholder width="6/12" />
-                </Stack>
-                <Doc.CodeSnippet
-                  codeSnippet={codeSnippet}
-                  showLineNumbers={false}
-                />
+      <Doc.Checkboxes
+        checkboxes={gapCheckboxes}
+        setCheckboxes={setGapCheckboxes}
+      >
+        {(checked) => (
+          <Doc.Block
+            subTitle="Gap"
+            exampleWithCode={
+              <Stack gap={`${checked?.value}`} alignItems="center">
+                <Doc.Placeholder width="full" />
+                <Doc.Placeholder width="full" />
+                <Doc.Placeholder width="full" />
               </Stack>
-            );
-            return (
-              <Stack alignItems={checked?.value}>
+            }
+          />
+        )}
+      </Doc.Checkboxes>
+
+      <Doc.Checkboxes
+        checkboxes={alignCheckboxes}
+        setCheckboxes={setAlignCheckboxes}
+      >
+        {(checked) => (
+          <Doc.Block
+            subTitle="Alignment"
+            exampleWithCode={
+              <Stack alignItems={`${checked?.value}`}>
                 <Doc.Placeholder width="6/12" />
                 <Doc.Placeholder width="6/12" />
                 <Doc.Placeholder width="6/12" />
               </Stack>
-            );
-          }}
-        </Doc.Checkboxes>
-      </Doc.Example>
+            }
+          />
+        )}
+      </Doc.Checkboxes>
     </Doc>
   );
 };
