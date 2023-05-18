bae0466f1626ffa63811d2507ea897ce1c3b1032
Matspo <sporismatija12345@gmail.com>
2023-05-18 13:08:46 +0200

    gql req and rq

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,3 +1,10 @@
+import React from 'react'
+
 export default function Home() {
+  React.useEffect(() => {
+    fetch('http://localhost:4000/api/accounts')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }, [])
   return null
 }
