7a6288799c847195216ae56d68e377c35ae6e8dc
Matspo <sporismatija12345@gmail.com>
2023-05-19 13:43:41 +0200

    delete history release mutation

---
+++ apps/kodi_straga/lib/graphql/types/inputs/delete_history_release_input.ex
@@ -0,0 +1,8 @@
+
+defmodule Graphql.Types.Inputs.DeleteHistoryReleaseInput do
+  use Absinthe.Schema.Notation
+
+  input_object :delete_history_release_input do
+    field(:id, non_null(:id))
+  end
+end
