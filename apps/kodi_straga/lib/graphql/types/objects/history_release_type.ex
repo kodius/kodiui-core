6c95ac92984a6cb07f47e21f4d30e19f319237c3
ante <twogamers1950@gmail.com>
2023-05-24 13:51:31 +0200

    typo

--- apps/kodi_straga/lib/graphql/types/objects/history_release_type.ex
+++ apps/kodi_straga/lib/graphql/types/objects/history_release_type.ex
@@ -1,12 +1,12 @@
 defmodule Graphql.Types.Objects.HistoryReleaseType do
   use Absinthe.Schema.Notation
 
-  object :history_relesase do
+  object :history_release do
     field :id, non_null(:id)
     field(:name, :string)
     field(:version, :string)
     field(:description, :string)
     field(:element_id, :id)
-    field(:example, :string)
+    # field(:example, :string)
   end
 end
