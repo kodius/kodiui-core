c614353e247e5fefb146078d0731f09d4b72a36f
dinoDanic <dino.danic@gmail.com>
2023-05-24 13:48:09 +0200

    c

--- apps/kodidocs/codegen.ts
+++ apps/kodidocs/codegen.ts
@@ -4,6 +4,7 @@ const config: CodegenConfig = {
   overwrite: true,
   schema: 'http://127.0.0.1:4000/api/graphql',
   // schema: 'http://116.203.201.51:4001/api/graphql',
+  // schema: '116.203.201.51:4001/api/graphql',
   documents: 'src/**/*.graphql',
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
---
+++ apps/kodidocs/src/features/sidebar/data/categories.ts
@@ -0,0 +1,5 @@
+import { Category } from '@gql/graphql'
+
+export const categories: Category[] = [
+  { name: 'Primitives', id: '0', elements: [{ name: 'Stack', id: '0' }] },
+]
