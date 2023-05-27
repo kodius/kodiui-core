ce6a1d28251cfe6dc0e315e34e483c038c194d8e
dinoDanic <dino.danic@gmail.com>
2023-05-27 23:28:25 +0200

    typography cli

--- apps/kodidocs/compress-components.sh
+++ apps/kodidocs/compress-components.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 dirs=(
-    'primitives:primitives'
     'typography:typography'
 )
 
@@ -16,7 +15,7 @@ for dir in "${dirs[@]}"; do
     cd "$initial_dir/components/$path"  # Change back to the initial working directory
     
     if [ $? -eq 0 ]; then  # Check if the directory change was successful
-        zip -r "$name.zip" . && mv "$name.zip" "$initial_dir/packages/cli/components"
+        zip -r "$name.zip" . && mv "$name.zip" "$initial_dir/../../packages/cli/components"
         echo -e "\033[32m $name was successfully compressed\033[0m"
     else
         echo -e "\033[31m Failed to change directory to components/$path\033[0m"
