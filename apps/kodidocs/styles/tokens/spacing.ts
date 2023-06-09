382fad88470f980445fba4502619f79ec64e3c9d
Adel Mrđanović <adelmrdanovic@Adels-MacBook-Pro.local>
2023-06-09 10:37:10 +0200

    Fix problem with spacing - tailwind

--- apps/kodidocs/styles/tokens/spacing.ts
+++ apps/kodidocs/styles/tokens/spacing.ts
@@ -43,3 +43,15 @@ export const gapVariants = cva("", {
 })
 
 export type GapVariants = VariantProps<typeof gapVariants>
+
+export const spacing = {
+  xxs: ".25rem",
+  xs: ".5rem",
+  sm: "1rem",
+  md: "1.25rem",
+  lg: "1.5rem",
+  xl: "1.75rem",
+  "2xl": "2rem",
+  "3xl": "3rem",
+  "5xl": "4rem",
+}
