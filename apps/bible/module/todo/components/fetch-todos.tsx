f3728c72d3f89ede625419d0424c55cce80d7dd3
Dino Danic <dino.danic@gmail.com>
2024-03-01 15:51:41 +0100

    modular approach

--- apps/bible/module/todo/components/fetch-todos.tsx
+++ apps/bible/module/todo/components/fetch-todos.tsx
@@ -5,9 +5,9 @@ export const FetchTodos = async () => {
   const { todos } = await todo.api.getTodos();
   return (
     <BasicList title="todos:">
-      {todos.map((item) => {
-        return <todo.components.TodoCard key={item.id} {...item} />;
-      })}
+      {todos.map((item) => (
+        <todo.components.TodoCard key={item.id} {...item} />
+      ))}
     </BasicList>
   );
 };
